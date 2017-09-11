1ddc00b0137f2cac4d7a6a8645ac5eb74c8dcf84
warut-vijit <warut.vijitbenjaronk@gmail.com>
2017-09-11 13:24:33 -0500

    recursive parse for directories

--- README.md
+++ README.md
@@ -3,16 +3,33 @@ Python parser for emails in Enron dataset
 
 ## Use
 Call `parse_email` function on a path name
+Generates a JSON parse for a single file, generates a list of JSON parses recursively through subdirectories for a directory.
 
 ## Returns:
+For a file:
 ```
 {
   'message': 'Thanks Jeff. \n\nMike/Jeff:  FYI---all of our people are ...',
   'cc': ['Andrew Makk', 'Mac McClelland', 'Jim Rountree', 'Yaser Tobeh'],
   'time': 'Sun, 8 Oct 2000 18:48:00 -0700 (PDT)',
   'subject': 'Re: Latest developments in Palestine',
-  'to_field': ['Jeffrey Hammad', 'Mike McConnell', 'Jeffrey A Shankman@ECT'],
-  'from_field': 'Rick Bergsieker', 
+  'recipient': ['Jeffrey Hammad', 'Mike McConnell', 'Jeffrey A Shankman@ECT'],
+  'sender': 'Rick Bergsieker', 
   'bcc': ['']
 }
+```
+
+For a directory:
+```
+[
+  {
+    'message': 'Thanks Jeff. \n\nMike/Jeff:  FYI---all of our people are ...',
+    'cc': ['Andrew Makk', 'Mac McClelland', 'Jim Rountree', 'Yaser Tobeh'],
+    'time': 'Sun, 8 Oct 2000 18:48:00 -0700 (PDT)',
+    'subject': 'Re: Latest developments in Palestine',
+    'recipient': ['Jeffrey Hammad', 'Mike McConnell', 'Jeffrey A Shankman@ECT'],
+    'sender': 'Rick Bergsieker', 
+    'bcc': ['']
+  }
+]
 ```
--- email-parse.py
+++ email-parse.py
@@ -1,13 +1,15 @@
 import re
+from os import path, listdir
 import sys
+import logging
 
 #
 # Precompiled patterns for performance
 #
 time_pattern = re.compile("Date: (?P<data>[A-Z][a-z]+\, \d{1,2} [A-Z][a-z]+ \d{4} \d{2}\:\d{2}\:\d{2} \-\d{4} \([A-Z]{3}\))")
 subject_pattern = re.compile("Subject: (?P<data>.*)")
-from_pattern = re.compile("X-From: (?P<data>.*)")
-to_pattern = re.compile("X-To: (?P<data>.*)")
+sender_pattern = re.compile("X-From: (?P<data>.*)")
+recipient_pattern = re.compile("X-To: (?P<data>.*)")
 cc_pattern = re.compile("X-cc: (?P<data>.*)")
 bcc_pattern = re.compile("X-bcc: (?P<data>.*)")
 msg_start_pattern = re.compile("\n\n", re.MULTILINE)
@@ -18,21 +20,39 @@ msg_end_pattern = re.compile("\n+.*\n\d+/\d+/\d+ \d+:\d+ [AP]M", re.MULTILINE)
 # Returns
 #
 def parse_email(pathname):
-    with open(pathname) as TextFile:
-        text = TextFile.read()
-        print(text)
-        time = time_pattern.search(text).group("data")
-        subject = subject_pattern.search(text).group("data")
-        from_field = from_pattern.search(text).group("data")
-        to_field = to_pattern.search(text).group("data").split(", ")
-        cc = cc_pattern.search(text).group("data").split(", ")
-        bcc = bcc_pattern.search(text).group("data").split(", ")
-        msg_start_iter = msg_start_pattern.search(text).end()
-        try:
-            msg_end_iter = msg_end_pattern.search(text).start()
-            message = text[msg_start_iter:msg_end_iter]
-        except AttributeError: # not a reply
-            message = text[msg_start_iter:]
-   
+    if path.isdir(pathname):
+        print(pathname)
+        emails = []
+        for child in listdir(pathname):
+            # only parse visible files
+            if child[0] != ".":
+                child_parse = parse_email(path.join(pathname, child))
+                if type(child_parse) == list and len(child_parse) > 0:
+                     emails += child_parse
+                elif type(child_parse) == dict:
+                     emails.append(child_parse)
+        return emails 
+    else:
+        print("file %s" % pathname)
+        with open(pathname) as TextFile:
+            text = TextFile.read()
+            try:
+                time = time_pattern.search(text).group("data")
+                subject = subject_pattern.search(text).group("data")
+                sender = sender_pattern.search(text).group("data")
+                recipient = recipient_pattern.search(text).group("data").split(", ")
+                cc = cc_pattern.search(text).group("data").split(", ")
+                bcc = bcc_pattern.search(text).group("data").split(", ")
+                msg_start_iter = msg_start_pattern.search(text).end()
+                try:
+                    msg_end_iter = msg_end_pattern.search(text).start()
+                    message = text[msg_start_iter:msg_end_iter]
+                except AttributeError: # not a reply
+                    message = text[msg_start_iter:]
+            except AttributeError:
+                logging.error("Failed to parse %s" % pathname) 
+                return None
 
-    return {'time': time, 'subject': subject, 'from_field': from_field, 'to_field': to_field, 'cc': cc, 'bcc': bcc, 'message': message}
+        return {'time': time, 'subject': subject, 'sender': sender, 'recipient': recipient, 'cc': cc, 'bcc': bcc, 'message': message}
+
+print(parse_email(sys.argv[1]))
