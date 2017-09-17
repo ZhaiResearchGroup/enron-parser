9076254bfba8da60827559e1a686e78a4ae7a6d8
warut-vijit <warut.vijitbenjaronk@gmail.com>
2017-09-17 18:41:13 -0500

    thread identification

--- README.md
+++ README.md
@@ -2,34 +2,23 @@
 Python parser for emails in Enron dataset
 
 ## Use
-Call `parse_email` function on a path name
+Call `python parse_email.py` on a path name for directory or file
 Generates a JSON parse for a single file, generates a list of JSON parses recursively through subdirectories for a directory.
 
 ## Returns:
-For a file:
+Creates or overwrites the following files:
+
+`threads.json`
 ```
-{
- 'message': 'Thanks Jeff. Mike/Jeff: FYI---all of our people are ...',
- 'cc': ['Andrew Makk', 'Mac McClelland', 'Jim Rountree', 'Yaser Tobeh'],
- 'time': 'Sun, 8 Oct 2000 18:48:00 -0700 (PDT)',
- 'subject': 'Re: Latest developments in Palestine',
- 'recipient': ['Jeffrey Hammad', 'Mike McConnell', 'Jeffrey A Shankman@ECT'],
- 'sender': 'Rick Bergsieker', 
- 'bcc': ['']
-}
+{"": 0, "Weekly Environmental Activity Report-Roswell Area": 2, "Weekly Activity Report, Roswell Area": 1, "Weekly Environemtnal Activity Report": 6, "Weekly Environmental Acitivity Report Roswell ARea": 10, "weekly environmental activity report-Roswell Area": 4, "Environmental findings, Koch South Texas Records Review": 3, "Riedel's Weekly Information September 7, 2001": 5, "Weekly Activity Report Roswell Area": 7, "Weekly Envoironmental Activity Report": 8, "Weekly Report, Roswell Area": 9}
 ```
 
-For a directory:
+`users.json`
 ```
-[
- {
- 'message': 'Thanks Jeff. Mike/Jeff: FYI---all of our people are ...',
- 'cc': ['Andrew Makk', 'Mac McClelland', 'Jim Rountree', 'Yaser Tobeh'],
- 'time': 'Sun, 8 Oct 2000 18:48:00 -0700 (PDT)',
- 'subject': 'Re: Latest developments in Palestine',
- 'recipient': ['Jeffrey Hammad', 'Mike McConnell', 'Jeffrey A Shankman@ECT'],
- 'sender': 'Rick Bergsieker', 
- 'bcc': ['']
- }
-]
+{"jim.lynch@enron.com": 5, "larry.campbell@enron.com": 0, "gary.fuller@enron.com": 12, "randy.belyeu@enron.com": 10, "william.kendrick@enron.com": 1, "rich.jolly@enron.com": 3, "joe.richards@enron.com": 11, "rick.loveless@enron.com": 7, "dave.miller@enron.com": 6, "scott.clark@enron.com": 2, "mike.riedel@enron.com": 8, "randy.howard@enron.com": 4, "scott.jones@enron.com": 9}
+```
+
+`messages.json`
+```
+[{"bcc": [], "message": "The annual pollution prevention plans were completed and submitted to the State of Arizona. These plans are requried for the three large quantity generators of hazardous waste in Arizona. Annual hazardous waste fees were paid and submitted to the State of New Mexico for four facilities in New Mexico. A conference call was held with Houston Legal and Environmetnal Affairs to discuss the alleged NOV issued for the P-1 C/S which involved a turbine like for like replacement activity. Data has begun to be collected to substantiate Transwestern's position concerning the proposed action by the State of New Mexico. A field trip was made to the Air Quality Bureau to copy all files relevant to the P-1 C/S and will be delivered to the contract attorney who will be assisting Transwestern with this issue. Copies of the New Mexico EOTT air permits were received. Comparisons will be made with the nearly completed EOTT tank database spreadsheet to determine status of permit compliance with each air permit.", "sender": 0, "thread": 10, "recipient": [7, 8, 9], "cc": [], "time": "Thu, 28 Jun 2001 13:10:00 -0700 (PDT)"}]
 ```
--- email-parse.py
+++ email-parse.py
@@ -1,7 +1,6 @@
 import re
 from os import path, listdir
 import sys
-import logging
 import json
 
 #
@@ -11,25 +10,29 @@ time_pattern = re.compile("Date: (?P<data>[A-Z][a-z]+\, \d{1,2} [A-Z][a-z]+ \d{4
 subject_pattern = re.compile("Subject: (?P<data>.*)")
 sender_pattern = re.compile("From: (?P<data>.*)")
 recipient_pattern = re.compile("To: (?P<data>.*)")
-cc_pattern = re.compile("X-cc: (?P<data>.*)")
-bcc_pattern = re.compile("X-bcc: (?P<data>.*)")
+cc_pattern = re.compile("cc: (?P<data>.*)")
+bcc_pattern = re.compile("bcc: (?P<data>.*)")
 msg_start_pattern = re.compile("\n\n", re.MULTILINE)
 msg_end_pattern = re.compile("\n+.*\n\d+/\d+/\d+ \d+:\d+ [AP]M", re.MULTILINE)
 
+#
+# Function: parse_email
+# Arguments: pathname - relative path of folder/file to be parsed
+#            orig     - whether this call is the original, used for writing to file
+# Returns: none
+# Effects: dumps json into file
+#
 feeds = []
-#
-# Accepts path as argument, returns parsed data as json
-# Returns
-#
-def parse_email(pathname):
+users = {}
+threads = {}
+def parse_email(pathname, orig=True):
     if path.isdir(pathname):
         print(pathname)
         emails = []
         for child in listdir(pathname):
             # only parse visible files
             if child[0] != ".":
-                parse_email(path.join(pathname, child))
-        return emails 
+                parse_email(path.join(pathname, child), False)
     else:
         print("file %s" % pathname)
         with open(pathname) as TextFile:
@@ -52,10 +55,45 @@ def parse_email(pathname):
             except AttributeError:
                 logging.error("Failed to parse %s" % pathname) 
                 return None
+            # get user and thread ids
+            sender_id = get_or_allocate_uid(sender)
+            recipient_id = [get_or_allocate_uid(u) for u in recipient if u!=""]
+            cc_ids = [get_or_allocate_uid(u) for u in cc if u!=""]
+            bcc_ids = [get_or_allocate_uid(u) for u in bcc if u!=""]
+            thread_id = get_or_allocate_tid(subject)
+        entry =  {"time": time, "thread": thread_id, "sender": sender_id, "recipient": recipient_id, "cc": cc_ids, "bcc": bcc_ids, "message": message}
+        feeds.append(entry)
+    if orig:
+        try:
+            with open('messages.json', 'w') as f:
+                json.dump(feeds, f)
+            with open('users.json', 'w') as f:
+                json.dump(users, f)
+            with open('threads.json', 'w') as f:
+                json.dump(threads, f)
+        except IOError:
+            print("Unable to write to output files, aborting")
+            exit(1)
 
-        with open('json.json', 'w') as f:
-            entry =  {"time": time, "subject": subject, "sender": sender, "recipient": recipient, "cc": cc, "bcc": bcc, "message": message}
-            feeds.append(entry)
-            json.dump(feeds, f)
-            
+#
+# Function: get_or_allocated_uid
+# Arguments: name - string of a user email
+# Returns: unique integer id
+#
+def get_or_allocate_uid(name):
+     if name not in users:
+         users[name] = len(users)
+     return users[name]
+
+#
+# Function: get_or_allocate_tid
+# Arguments: name - string of email subject line
+# Returns: unique integer id
+#
+def get_or_allocate_tid(name):
+    parsed_name = re.sub("Re: ", "", name)
+    if parsed_name not in threads:
+        threads[parsed_name] = len(threads)
+    return threads[parsed_name]
+
 parse_email(sys.argv[1])
